d81e882e4c4202727a2a7063aefea14ac75b7c33
Luciano Cavallini Cyrillo <lccyrillo@gmail.com>
2021-08-14 17:34:45 -0300

    incluindo o banco de dados em memória

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,10 +1,12 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Ativo }  from "./pages/ativos/shared/ativo.model";
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDB(){
-        const ativos = [
-            { id: 1, sigla: 'ITUB4', nome: 'Itaú Unibanco', descricao_cnpj: '60.872.504/0001-23', tipo_ativo: '1' },
-            { id: 2, sigla: 'B3SA3', nome: 'B3 S.A. - BRASIL. BOLSA. BALCÃO', descricao_cnpj: '09.346.601/0001-25', tipo_ativo: '1' }
+        const ativos: Ativo[] = [
+            { id: 1, sigla: 'ITUB4', nome: 'Itaú Unibanco', descricao_cnpj: '60.872.504/0001-23', tipo_ativo: 1 },
+            { id: 2, sigla: 'B3SA3', nome: 'B3 S.A. - BRASIL. BOLSA. BALCÃO', descricao_cnpj: '09.346.601/0001-25', tipo_ativo: 1 },
+            { id: 3, sigla: 'BBDC4', nome: 'Bradesco', descricao_cnpj: "60.872.504/0001-23", tipo_ativo: 1}
 
         ];
         return { ativos }
---
+++ src/app/pages/ativos/shared/ativo.model.ts
@@ -0,0 +1,8 @@
+export class Ativo {
+    constructor(
+        public id?:number,
+        public nome?: string,
+        public descricao_cnpj?: string,
+        public tipo_ativo?: number
+    ) {}
+}
